d674687f56ce7702278dc4b587e7a22ee1137e79
jankit218 <jankit218@gmail.com>
2022-08-05 15:30:53 +0100

    updated

--- application.py
+++ application.py
@@ -720,4 +720,5 @@ for code in default_exceptions:
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+#    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
